5c626e3d43dfa31a771e59d1fcdebcc31bfefe9d
joelgoughbecker <116875565+joelgoughbecker@users.noreply.github.com>
2022-11-11 13:03:50 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,25 @@
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(400, 400);
     canvas.parent("p5container");
-}
 
+
+}
 function draw() {
-    background(220);
+  background(220);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
-}
+    if(mouseX < width / 2){
+    console.log('left'); //write somthing in console
+    ellipse(200, 200, 200, 200)
+    fill('red');
+    arc(200, 250, 100, 100, PI, TWO_PI);
+    
+    } else{
+      console.log('right');
+      ellipse(200, 200, 200, 200)
+    fill('green');
+    
+    arc(200, 200, 100, 100, TWO_PI, PI);
+   
+    }
+  }
